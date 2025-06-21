a2feeb907342dca93a51eec2e6cb808cc30e22b4
Dominic Smart <dominicsmart99@gmail.com.com>
2025-06-21 19:30:35 -0400

    Add error handling component and style button in globals.css

---
+++ src/app/error.jsx
@@ -0,0 +1,18 @@
+"use client";
+import React, { useEffect } from "react";
+
+export default function error({ error, reset }) {
+  useEffect(() => {
+    console.error("An error occurred:", error);
+    // Optionally, you can log the error to an external service here
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-20">
+      <h1>Something went wrong. Please try again later.</h1>
+      <button onClick={() => reset()} className="hover:text-amber-400 ">
+        Try Again
+      </button>
+    </div>
+  );
+}
